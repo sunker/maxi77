177e01024822939d63a66b6e0e11355cc39db87f
Erik Sundell <erik.sundell87@gmail.com>
2016-10-03 10:15:27 +0200

    Prep new widget

--- public/scripts/dashboard/dashboardController.js
+++ public/scripts/dashboard/dashboardController.js
@@ -1,5 +1,5 @@
-angular.module('HomeCtrl', []).controller('DashboardController', function($scope, $rootScope) {
+angular.module('HomeCtrl', []).controller('DashboardController', function($scope, geoService) {
 
 	// $rootScope.dayText = new Date().toSwedishDay();	
-
+	$scope.speed = geoService.getCurrentSpeed().toFixed(2);
 }); 
